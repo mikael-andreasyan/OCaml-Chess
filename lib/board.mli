37024a4bc3e95fb7286940c593239085588bd63b
ma2384 <ma2384@cornell.edu>
2025-03-26 16:14:54 -0400

    Fixed interface to have 0-7 represent the letters a-h

--- lib/board.mli
+++ lib/board.mli
@@ -14,7 +14,8 @@ val make_move : t -> int * int -> int * int -> bool
    illegal, then the function returns false and makes no changes to the board.
    Otherwise, it mutates the current board to represent the move, changes board
    state, and advances the turn. Requires: [file_st, rank_st] and
-   [file_end, rank_end] are valid positions on a chess board*)
+   [file_end, rank_end] are valid positions on a chess board. For this function,
+   the numbers 0-7 represent the letters a-h.*)
 
 val make_board : unit -> t
 (**Creates a fresh instance of the board*)
--- lib/piece.mli
+++ lib/piece.mli
@@ -21,9 +21,9 @@ val valid_pattern : int * int -> int * int -> t -> bool
    able to check if a certain move is legal however (for example other pieces
    blocking the path or a piece not being able to move because it would put the
    king in check.). Note: The [file] is the letter associated with the column
-   that the piece is on. For this function, the numbers 1-8 represent the
+   that the piece is on. For this function, the numbers 0-7 represent the
    letters a-h. Requires: [file_st, rank_st] and [file_end, rank_end] are valid
-   positions on a chess board**)
+   positions on a chess board*)
 
 val get_color : t -> color
 (**[get_color piece] returns the color of the piece*)
