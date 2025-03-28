308a6596bbe3dfc2c6504d92372b898b1593c564
AndrewK <ajk345@cornell.edu>
2025-03-27 21:35:08 -0400

    Implemented: piece movement

--- lib/piece.ml
+++ lib/piece.ml
@@ -15,7 +15,26 @@ type t = {
   color : color;
 }
 
-let valid_pattern _ _ _ = true
+let valid_pattern (file_st, rank_st) (file_end, rank_end) piece =
+  let rank_diff = rank_end - rank_st in
+  let file_diff = file_end - file_st in
+  match piece.piece with
+  | Pawn -> (
+      match piece.color with
+      | White -> rank_diff = 1 || (rank_st = 1 && rank_diff = 2)
+      | Black -> rank_diff = -1 || (rank_st = 6 && rank_diff = -2))
+  | Knight ->
+      (Int.abs rank_diff = 2 && Int.abs file_diff = 1)
+      || (Int.abs rank_diff = 1 && Int.abs file_diff = 2)
+  | Bishop -> Int.abs rank_diff = Int.abs file_diff
+  | Rook ->
+      (file_diff <> 0 && rank_diff = 0) || (rank_diff <> 0 && file_diff = 0)
+  | King -> Int.abs rank_diff = 1 || Int.abs file_diff = 1
+  | Queen ->
+      (file_diff <> 0 && rank_diff = 0)
+      || (rank_diff <> 0 && file_diff = 0)
+      || Int.abs rank_diff = Int.abs file_diff
+
 let get_color piece = piece.color
 let get_type piece = piece.piece
 
--- lib/piece.mli
+++ lib/piece.mli
@@ -17,13 +17,13 @@ val valid_pattern : int * int -> int * int -> t -> bool
 (**[valid_move (file_st, rank_st) (file_end, rank_end) piece] returns if a given
    move starting from [file_st, rank_st] to [file_end,rank_end] matches the
    movement pattern of the specified piece. For pawns, this function assumes
-   both diagonal and straight movements match the pattern. This function is not
-   able to check if a certain move is legal however (for example other pieces
-   blocking the path or a piece not being able to move because it would put the
-   king in check.). Note: The [file] is the letter associated with the column
-   that the piece is on. For this function, the numbers 0-7 represent the
-   letters a-h. Requires: [file_st, rank_st] and [file_end, rank_end] are valid
-   positions on a chess board*)
+   only straight movements match the pattern, so diagonal movements need to be
+   adjusted. This function is not able to check if a certain move is legal
+   however (for example other pieces blocking the path or a piece not being able
+   to move because it would put the king in check.). Note: The [file] is the
+   letter associated with the column that the piece is on. For this function,
+   the numbers 0-7 represent the letters a-h. Requires: [file_st, rank_st] and
+   [file_end, rank_end] are valid positions on a chess board*)
 
 val get_color : t -> color
 (**[get_color piece] returns the color of the piece*)
